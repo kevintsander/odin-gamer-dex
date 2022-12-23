af62255b587583ea158473fb1ebee98d6a1b54b3
Kevin Sander <kevintsander@gmail.com>
2022-12-23 13:57:48 -0500

    added user registration styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,27 @@ class User < ApplicationRecord
     false
   end
 
+  def display_name
+    p self
+    puts "name #{name} email #{email} username #{username}"
+    return name unless name.blank?
+    return username unless username.blank?
+    return email unless email.blank?
+  end
+
   def self.from_omniauth(auth)
+    p 'AUTH'
+    p auth
+    p 'AUTH INFO'
+    p auth.info
+    p 'AUTH EXTRA'
+    p auth.extra
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       p auth.info
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name # assuming the user model has a name
-      user.username = auth.info.nickname # assuming the user model has a username
+      user.username = auth.screen_name # assuming the user model has a username
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
