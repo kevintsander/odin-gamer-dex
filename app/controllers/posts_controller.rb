3c28eec3393c0b4a7547551c1afc1b525a5bf266
Kevin Sander <kevintsander@gmail.com>
2022-12-30 14:04:10 -0500

    allow post edits in place

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_user, only: %i[index create edit show update]
+  before_action :set_post, only: %i[edit show update]
+
   def index
     @user = User.find(params[:user_id])
     @page_size = 10
@@ -11,8 +14,9 @@ class PostsController < ApplicationController
     end
   end
 
+  def show; end
+
   def create
-    @user = User.find(params[:user_id])
     @post = @user.posts.build(post_params)
 
     respond_to do |format|
@@ -24,9 +28,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to user_post_path(@user, @post) }
+      else
+        format.html { render :index, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:body)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+  def display_date_time(time)
+    elapsed_seconds = (Time.now - time).seconds
+    if elapsed_seconds < 60
+      "#{elapsed_seconds.round} seconds ago"
+    elsif elapsed_seconds < 3600
+      "#{(elapsed_seconds / 60).round} minutes ago"
+    elsif time.today?
+      "Today @  #{time.strftime('%k %M %p')}"
+    else
+      "#{time.strftime('%b %e')} @ #{time.strftime('%k %M %p')}"
+    end
+  end
 end
