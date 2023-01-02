fd17799754eb68611053a8864e9deee8a9e2a810
Kevin Sander <kevintsander@gmail.com>
2023-01-02 15:39:18 -0500

    added tailwind icons

--- app/javascript/controllers/attachment_preview_controller.js
+++ app/javascript/controllers/attachment_preview_controller.js
@@ -3,18 +3,23 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["attachmentInput", "imgContainer"];
   static values = { width: Number, height: Number };
+  static classes = ["wrapper"];
 
   setPreviewImages() {
-    let images = [];
+    let wrappers = [];
+    console.log(this.wrapperClasses);
     Array.from(this.attachmentInputTarget.files).forEach((file) => {
+      let wrapper = document.createElement("div");
       let img = document.createElement("img");
       img.src = URL.createObjectURL(file);
-      img.style.objectFit = "contain";
-      img.width = this.widthValue;
-      img.height = this.heightvalue;
-      images.push(img);
-      this.imgContainerTarget.appendChild(img);
+      img.style.maxWidth = this.widthValue + "px";
+      img.style.maxHeight = this.heightValue + "px";
+      wrapper.appendChild(img);
+      wrapper.classList.add(...this.wrapperClasses);
+      wrappers.push(wrapper);
     });
-    this.imgContainerTarget.replaceChildren(...images);
+    this.imgContainerTarget.replaceChildren(...wrappers);
+
+    this.imgContainerTarget.appendChild;
   }
 }
