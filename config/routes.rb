536cc1fcec010c08f6f82fb745b76c346322ac1e
Kevin Sander <kevintsander@gmail.com>
2023-01-08 13:04:31 -0500

    removed user index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
              controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
 
-  resources :users, only: %i[show index] do
+  resources :users, only: %i[show] do
     resources :posts
     resources :user_relationships, only: %i[create update destroy], param: :friend_id
     resources :games, only: %i[index create destroy]
