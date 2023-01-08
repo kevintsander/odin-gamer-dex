ca2f21e54ddd2575e05cad6359399b4606774be4
Kevin Sander <kevintsander@gmail.com>
2023-01-08 17:16:22 -0500

    improved title bar styling

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,11 +2,17 @@ module UsersHelper
   def relationship_image(user, relationship, size)
     case relationship_setting(user, relationship)
     when 'self'
-      inline_svg_tag 'star.svg', class: 'stroke-amber-400 fill-amber-400', size:
+      inline_svg_tag 'star.svg', class: 'stroke-amber-400 fill-amber-400',
+                                 title: "Hey, it's you!",
+                                 size:
     when 'pending me', 'pending other'
-      inline_svg_tag 'question.svg', class: 'stroke-yellow-200', size:
+      inline_svg_tag 'question.svg', class: 'stroke-yellow-200',
+                                     title: "Your friendship with #{user.display_name} is pending",
+                                     size:
     when 'friends'
-      inline_svg_tag 'friends.svg', class: 'stroke-lime-200 fill-lime-200', size:
+      inline_svg_tag 'friends.svg', class: 'stroke-lime-200 fill-lime-200',
+                                    title: "You're friends with #{user.display_name}",
+                                    size:
     end
   end
 end
